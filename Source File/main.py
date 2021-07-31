17a6e36cd13cc46ff242e31b3a8a05d4bcf4c852
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 01:36:22 +0800

    Update main.py

--- Source File/main.py
+++ Source File/main.py
@@ -2,3 +2,15 @@
     This is the main script where everything are been implemented here and execute it
     Making the game flow by calling function and module
 """
+def main():
+    #For testing only:
+    import BattleGround as battleGround
+    
+    AI_Win = 1
+    playerWin = 2
+    AI_ShipLeft = 3
+    playerShipLeft = 2
+    
+    battleGround.printMap(AI_Win, playerWin, AI_ShipLeft, playerShipLeft)
+    
+main()
