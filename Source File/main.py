4a060c1c72911632e798414cb1dc582b638d1797
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 16:58:33 +0800

    Update main.py

--- Source File/main.py
+++ Source File/main.py
@@ -2,16 +2,57 @@
     This is the main script where everything are been implemented here and execute it
     Making the game flow by calling function and module
 """
+
+def shipTest():
+    # Doesn't Work!
+    import Ship as Ship1
+    import Ship as Ship2
+    import Ship as Ship3
+    
+    Ship1.posX = 4
+    print(Ship2.posX) # 4
+    print(Ship3.posX) # 4
+
+
+playerShip1 = {"Pos":[0, 0]}
+playerShip2 = {"Pos":[0, 0]}
+playerShip3 = {"Pos":[0, 0]}
+
+def playerInput():
+    for i in range(3):
+        
+        ship1_string = "Ship 1: -"
+        ship2_string = "Ship 2: -"
+        ship3_string = "Ship 3: -"
+        
+        if (i == 0): ship1_string = "> Ship 1: <"
+        elif (i == 1): ship2_string = "> Ship 2: <"
+        else: ship3_string = "> Ship 3: <"
+        
+        print(ship1_string + "\n\n" + ship2_string + "\n\n" + ship3_string + "\n")
+        print("Battlefield Size (9x9) -> Please enter between 1 to 9\n")
+        
+        posX = input("Place ship " + str(i + 1) + " to (PosX): "); print()
+        posY = input("Place ship " + str(i + 1) + " to (PosY): "); print()
+        
+        if (i == 0):
+            playerShip1["Pos"][0] = posX
+            playerShip1["Pos"][1] = posY
+        elif(i == 1):
+            playerShip2["Pos"][0] = posX
+            playerShip2["Pos"][1] = posY
+        else:
+            playerShip3["Pos"][0] = posX
+            playerShip3["Pos"][1] = posY
+        
+
 def main():
-    #For testing only:
-    import BattleGround as battleGround
-    
-    AI_Win = 1
-    playerWin = 2
-    AI_ShipLeft = 3
-    playerShipLeft = 2
-    
-    battleGround.printMap(AI_Win, playerWin, AI_ShipLeft, playerShipLeft)
-    battleGround.goTo(20, 20, "A")
+    while True:
+        
+        print("Welcome To Battle-Ship War Game\n")
+        
+        playerInput()
+        
+        break
     
 main()
