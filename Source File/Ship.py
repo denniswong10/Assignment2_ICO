bbd001ab2a55935615525b738935b779dd10b71a
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 16:58:31 +0800

    Update Ship.py

--- Source File/Ship.py
+++ Source File/Ship.py
@@ -2,3 +2,6 @@
     This is a script for creating ship to both player where there used to identify ship
 """
 
+# Please remove this module, its useless as it can only be used once even if I import it multiple times
+posX = 0
+posY = 0
