f6af17da38b18dce96eee1eeecabbd93b078768c
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 19:22:09 +0800

    Update main.py

--- Source File/main.py
+++ Source File/main.py
@@ -2,36 +2,52 @@
     This is the main script where everything are been implemented here and execute it
     Making the game flow by calling function and module
 """
-def shipTest():
-    # Doesn't Work!
-    import Ship as Ship1
-    import Ship as Ship2
-    import Ship as Ship3
-    
-    Ship1.posX = 4
-    print(Ship2.posX) # 4
-    print(Ship3.posX) # 4
-    
-    
 import Player as player
 import AI
 
+isQuit = False
+
 def playerPosXY():
+    ship1_string = "Ship 1: -"
+    ship2_string = "Ship 2: -"
+    ship3_string = "Ship 3: -"
+    
     for i in range(3):
         
-        ship1_string = "Ship 1: -"
-        ship2_string = "Ship 2: -"
-        ship3_string = "Ship 3: -"
+        print("\n<Press 'q' to exit>")
         
-        if (i == 0): ship1_string = "> Ship 1: <"
-        elif (i == 1): ship2_string = "> Ship 2: <"
-        else: ship3_string = "> Ship 3: <"
+        if (i == 0): 
+            ship1_string = "> Ship 1: <"
+        elif (i == 1):
+            ship1_string = "Ship 1: (" + str(player.playerShip1["Pos"][0]) + ", " + str(player.playerShip1["Pos"][1]) + ")"
+            ship2_string = "> Ship 2: <"
+        else:
+            ship1_string = "Ship 1: (" + str(player.playerShip1["Pos"][0]) + ", " + str(player.playerShip1["Pos"][1]) + ")"
+            ship2_string = "Ship 1: (" + str(player.playerShip2["Pos"][0]) + ", " + str(player.playerShip2["Pos"][1]) + ")"
+            ship3_string = "> Ship 3: <"
         
-        print(ship1_string + "\n\n" + ship2_string + "\n\n" + ship3_string + "\n")
-        print("Battlefield Size (9x9) -> Please enter between 1 to 9\n")
+        print("\n" + ship1_string + "\n\n" + ship2_string + "\n\n" + ship3_string)
+        print("\nBattlefield Size (9x9) -> Please enter between 1 to 9")
         
-        posX = input("Place ship " + str(i + 1) + " to (PosX): "); print()
-        posY = input("Place ship " + str(i + 1) + " to (PosY): "); print()
+        global isQuit
+        
+        validValue = False
+        while (validValue == False):
+            posX = input("\nPlace ship " + str(i + 1) + " to (PosX): ")
+            if (posX.lower() == "q"): isQuit = True; break
+            if ((not posX.isdecimal()) and (not (1 <= int(float(posX)) <= 9))): print("\nInvalid Input!"); continue
+            validValue = True
+        if (isQuit == False): posX = int(float(posX))
+        if (isQuit == True): break    
+    
+        validValue = False    
+        while (validValue == False):
+            posY = input("\nPlace ship " + str(i + 1) + " to (PosY): ")
+            if (posY.lower() == "q"): isQuit = True; break
+            if ((not posY.isdecimal()) and (not (1 <= int(float(posY)) <= 9))): print("\nInvalid Input!"); continue
+            validValue = True
+        if (isQuit == False): posY = int(float(posY))
+        if (isQuit == True): break
         
         if (i == 0):
             player.playerShip1["Pos"][0] = posX
@@ -51,7 +67,8 @@ def AI_PosXY():
     
     for i in range(3):
         
-        for playerShip in (player.playerShips): # Checking if the positions generated are the same as the ones player inputted
+        # Checking if the positions generated are the same as the ones player inputted, if yes then generate again
+        for playerShip in (player.playerShips):
             for index in range(2):
                 while (playerShip["Pos"][index] == posX):
                     posX = random.randrange(1, 10)
@@ -69,16 +86,22 @@ def AI_PosXY():
             AI.AI_ship3["Pos"][1] = posY
 
 def main():
-    while True:
+    global isQuit
+    
+    while ((isQuit == False) and (player.shipLeft != 0) and (AI.shipLeft != 0)):
         
         import BattleGround as battleGround
         
-        print("Welcome To Battle-Ship War Game\n")
+        print("Welcome To Battle-Ship War Game")
         
         playerPosXY()
+        if (isQuit == True): break
         AI_PosXY()
-        battleGround.generateMap(player.win, player.shipLeft, AI.win, AI.shipLeft)
+        battleGround.generateMap()
         
         break
+        
+    if (player.shipLeft == 0): print("\nYou Lost :(")
+    elif (AI.shipLeft == 0): print("\nYou Won!")
     
 main()
