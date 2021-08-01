e893be26ae4b2fab4a119e9c17d5979ba361627f
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 18:04:23 +0800

    Update BattleGround.py

--- Source File/BattleGround.py
+++ Source File/BattleGround.py
@@ -2,7 +2,7 @@
     This is a script for creating battlefield to all object that need to grid their ship
 """
 
-def printMap(playerWin, playerShipLeft, AI_win, AI_shipLeft):
+def generateMap(playerWin, playerShipLeft, AI_win, AI_shipLeft):
     print("----1----2----3----4----5----6----7----8----9----    BOT Wins: " + str(AI_win) + "\n")
     for i in range(9):
         print(str(i + 1) + "|                                             |" + str(i + 1), end = (("    Ship Left: " + ((str(AI_shipLeft) + "\n") if (i == 0) else (str(playerShipLeft) + " \n"))) if ((i == 0) or (i == 8)) else "\n"))
