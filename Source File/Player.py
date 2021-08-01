787d898a5e1c6a09ba1affdd6c7406e47e307b02
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 17:57:17 +0800

    Update Player.py

--- Source File/Player.py
+++ Source File/Player.py
@@ -2,5 +2,10 @@
     This is a script for player that need to be control and make a plot of the cannon fire.
 """
 
+playerShip1 = {"Pos":[0, 0]}
+playerShip2 = {"Pos":[0, 0]}
+playerShip3 = {"Pos":[0, 0]}
+playerShips = [playerShip1, playerShip2, playerShip3]
+
 win = 0
 shipLeft = 3
