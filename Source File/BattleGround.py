c1e7e950e45d558d9569862cd7d67e4eb9b717d1
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-01 19:22:05 +0800

    Update BattleGround.py

--- Source File/BattleGround.py
+++ Source File/BattleGround.py
@@ -2,13 +2,15 @@
     This is a script for creating battlefield to all object that need to grid their ship
 """
 
-def generateMap(playerWin, playerShipLeft, AI_win, AI_shipLeft):
-    print("----1----2----3----4----5----6----7----8----9----    BOT Wins: " + str(AI_win) + "\n")
+import Player as player
+import AI
+
+def generateMap():
+    print("\n----1----2----3----4----5----6----7----8----9----    BOT Wins: " + str(AI.win))
     for i in range(9):
-        print(str(i + 1) + "|                                             |" + str(i + 1), end = (("    Ship Left: " + ((str(AI_shipLeft) + "\n") if (i == 0) else (str(playerShipLeft) + " \n"))) if ((i == 0) or (i == 8)) else "\n"))
+        print("\n" + str(i + 1) + "|                                             |" + str(i + 1), end = (("    Ship Left: " + ((str(AI.shipLeft)) if (i == 0) else str(player.shipLeft))) if ((i == 0) or (i == 8)) else ""))
         print()
-    print("----1----2----3----4----5----6----7----8----9----    Player Wins: " + str(playerWin) + "\n")
+    print("\n----1----2----3----4----5----6----7----8----9----    Player Wins: " + str(player.win) + "\n")
     
 def goTo(x, y, text):
-    #Not done
-    print("Hello World")
+    print("\nHello")
