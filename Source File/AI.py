c23bade9b15b3fbf3643587323b6fb9db98fecb6
denniswong10 <denniswong10.rhythmspro@gmail.com>
2021-08-01 23:07:05 +0800

    Update by denniswong10

--- Source File/AI.py
+++ Source File/AI.py
@@ -1,9 +1,10 @@
-<<<<<<< Updated upstream
 """
     Make a script that can make a bot to plot ship and fire cannon
 """
-=======
-"""
-    Make a script that can make a bot to plot ship and fire cannon
-"""
->>>>>>> Stashed changes
+
+AI_ship1 = {"Pos":[0, 0]}
+AI_ship2 = {"Pos":[0, 0]}
+AI_ship3 = {"Pos":[0, 0]}
+
+win = 0
+shipLeft = 3
