29331e345a60e0eef34416426bcdb8486ce2d250
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2021-08-02 18:25:05 +0800

    update 1.0.0

--- Source File/AI.py
+++ Source File/AI.py
@@ -8,3 +8,30 @@ AI_ship3 = {"Pos":[0, 0]}
 
 win = 0
 shipLeft = 3
+
+def AI_PosXY():
+    import random
+    import Player as player
+    
+    posX = random.randrange(1, 10)
+    posY = random.randrange(1, 10)
+    
+    for i in range(3):
+        
+        # Checking if the positions generated are the same as the ones player inputted, if yes then generate again
+        for playerShip in (player.playerShips):
+            for index in range(2):
+                while (playerShip["Pos"][index] == posX):
+                    posX = random.randrange(1, 10)
+                while (playerShip["Pos"][index] == posY):
+                    posY = random.randrange(1, 10)
+                    
+        if (i == 0):
+            AI_ship1["Pos"][0] = posX
+            AI_ship1["Pos"][1] = posY
+        elif(i == 1):
+            AI_ship2["Pos"][0] = posX
+            AI_ship2["Pos"][1] = posY
+        else:
+            AI_ship3["Pos"][0] = posX
+            AI_ship3["Pos"][1] = posY
